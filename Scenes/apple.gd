785e772601dbf8e2f8147d67f38cf92aca9db7d3
unknown <berkeonver@icloud.com>
2024-08-08 14:57:42 +0300

    Hotfix: Interaction System

--- Scenes/apple.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-@onready var interaction_area = $InteractionArea
-
-func _ready():
-	interaction_area.interact = Callable(self,"apple_interact")
-	
-func apple_interact():
-	print(self.name)
